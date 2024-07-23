4534958b416cd3e1af2d0f42c6870394b59c1e3b
soubh96 <35345806+soubh96@users.noreply.github.com>
2024-07-23 14:47:03 +0530

    Update cpserver.js

--- cpserver.js
+++ cpserver.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
