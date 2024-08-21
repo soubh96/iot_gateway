2edc8e4549fa84489e054126ba0ac98932bee9c8
soubh96 <35345806+soubh96@users.noreply.github.com>
2024-08-21 14:40:02 +0530

    Update cpserver.js

--- cpserver.js
+++ cpserver.js
@@ -64,9 +64,9 @@ app.get('/latest-data', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     
     const responseData = {
-        temperature: latestData.temperature || 36,
-        moisture: latestData.moisture || 51,
-        electricalConductivity: latestData.electricalConductivity || 102
+        temperature: latestData.temperature || 23.90,
+        moisture: latestData.moisture || 15.10,
+        electricalConductivity: latestData.electricalConductivity || 51.20
     };
 
     res.json(responseData);
