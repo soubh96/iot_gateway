efeed5b17d575bfc8e516a25f95e52187647099d
soubh96 <35345806+soubh96@users.noreply.github.com>
2024-08-26 14:27:23 +0530

    Add files via upload

---
+++ models/dataModel.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+
+const dataSchema= new mongoose.Schema({
+    temperature:{type:Number,default:0},
+    moisture:{type:number,default:0},
+    electricalConductivity: { type: Number, default: 102 },
+    nitrogen: { type: Number, default: 0 },
+    phosphorus: { type: Number, default: 0 },
+    potassium: { type: Number, default: 0 }     
+
+
+});
+
+const Data = mongoose.model('Data',dataSchema)
+
+module.exports=Data;
