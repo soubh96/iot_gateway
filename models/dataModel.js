295cc3e2d7e5c3a072de25b30b9d33e111edb6a6
soubh96 <35345806+soubh96@users.noreply.github.com>
2024-08-27 10:36:35 +0530

    Update dataModel.js

--- models/dataModel.js
+++ models/dataModel.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 const dataSchema= new mongoose.Schema({
     temperature:{type:Number,default:0},
-    moisture:{type:number,default:0},
+    moisture:{type:Number,default:0},
     electricalConductivity: { type: Number, default: 102 },
     nitrogen: { type: Number, default: 0 },
     phosphorus: { type: Number, default: 0 },
